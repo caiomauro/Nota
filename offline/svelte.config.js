8f23c2eb505bb066bc8985606819418a1a05a4da
Caio Mauro <101437705+caiomauro@users.noreply.github.com>
2023-11-01 19:33:11 -0400

    Fixed build
    Build now works!

--- offline/svelte.config.js
+++ offline/svelte.config.js
@@ -11,7 +11,12 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter()
+    adapter: adapter(
+      {
+        fallback: 'app.html', // Replace with the path to your fallback page
+        // You can also set other options for the adapter here
+      }
+    )
   }
 };
 
